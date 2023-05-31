8b89a90d3a119cbe10f2bbd6a30bb18f6a4c84ec
silverwest8 <silverwest8@gmail.com>
2023-05-31 19:58:47 +0900

    update router

--- src/index.js
+++ src/index.js
@@ -1,31 +1,23 @@
-"use strict";
+'use strict';
 
-import express from "express";
+import express from 'express';
 import Routers from './routes/index.js';
-// import { sequelize } from "./models";
+import { sequelize } from './models/index.js';
 
 const app = express();
 const port = 3000;
 
-
-app.use((req, res) => {
-  console.log("logging for routers");
-  res.send("hi");
-});
-
 app.use(express.json());
-app.get("/", )
-app.use("/", Routers);
+app.use('/', Routers);
 
-
-// sequelize
-//   .sync({ force: false }) //true면 서버 실행마다 테이블 재생성
-//   .then(() => {
-//     console.log("Mysql Connecting Success with Sequelize");
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//   });
+sequelize
+  .sync({ force: false }) //true면 서버 실행마다 테이블 재생성
+  .then(() => {
+    console.log('Mysql Connecting Success with Sequelize');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 //---- 서버 시작
 app.listen(port, () => {
